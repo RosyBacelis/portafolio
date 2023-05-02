ea8fad866e9b2609ebe0fa4247d5fc58b2039739
RosyBacelis <a19111302@alumnos.uady.mx>
2023-05-01 23:06:52 -0600

    first commit

---
+++ Iris_Rosy/Iris_Flower_Dataset.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pickle
+
+st.write("""
+¡Esta aplicación predice **el tipo de flor de Iris** con diferentes modelos!
+
+Selecciona el modelo que le gustaría saber
+""")
+
+lin_model=pickle.load(open('lin_model.pkl','rb'))
+log_model=pickle.load(open('log_model.pkl','rb'))
+svm=pickle.load(open('svm.pkl','rb'))
+
+def classify(num):
+    if num<0.5:
+        return 'Setosa'
+    elif num <1.5:
+        return 'Versicolor'
+    else:
+        return 'Virginica'
+def main():
+    st.title("Iris Flower Dataset")
+    html_temp = """
+    <div style="background-color:teal ;padding:10px">
+    <h2 style="color:white;text-align:center;">Iris Classification</h2>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+    activities=['Linear Regression','Logistic Regression','SVM']
+    option=st.sidebar.selectbox('¿Qué modelo te gustaría usar?',activities)
+    st.subheader(option)
+    sl=st.slider('Select Sepal Length', 0.0, 10.0)
+    sw=st.slider('Select Sepal Width', 0.0, 10.0)
+    pl=st.slider('Select Petal Length', 0.0, 10.0)
+    pw=st.slider('Select Petal Width', 0.0, 10.0)
+    inputs=[[sl,sw,pl,pw]]
+    if st.button('Classify'):
+        if option=='Linear Regression':
+            st.success(classify(lin_model.predict(inputs)))
+        elif option=='Logistic Regression':
+            st.success(classify(log_model.predict(inputs)))
+        else:
+           st.success(classify(svm.predict(inputs)))
+
+
+if __name__=='__main__':
+    main()
+
